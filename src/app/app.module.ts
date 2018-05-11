7ab6b7b16895fbdf2007dbb3941cbba6838d1415
Braden Brown <rubikdude@gmail.com>
2018-05-10 20:05:55 -0600

    asdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,8 @@ import { ProductService } from './product.service';
   declarations: [
     AppComponent,
     ProductPageComponent,
-    ProductDescriptionComponent
+    ProductDescriptionComponent,
+    ProductService
   ],
   imports: [
     BrowserModule,
